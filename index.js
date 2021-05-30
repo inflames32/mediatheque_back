1af577bfc1b412a081a8de97c27c221ca046d7d4
unknown <cahuzac.p@gmail.com>
2021-05-30 17:39:02 +0200

    first commit on dev

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
+const userRoutes = "./routes/user.routes";
+require("dotenv").config({ path: "./config/.env" });
+require("./config/db");
+const app = express();
+
+/* const mongoose = require("mongoose");
+
 mongoose.connect("mongodb://localhost:27017", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+}); */
 
 const router = require("./app/router");
-console.log(mongoose);
-const app = express();
+//console.log(mongoose);
 
-const PORT = 3000;
-
+const PORT = 5000;
+// routes
+app.use("/api/user", userRoutes);
 app.use(router);
+// server
 app.listen(PORT, () => {
   console.log("server up!");
 });
