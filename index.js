22d9226b5f5bef38fde87e10f204e1fb066314db
unknown <cahuzac.p@gmail.com>
2021-06-28 22:42:31 +0200

    adding process.env.CLIENT_URL

--- index.js
+++ index.js
@@ -7,18 +7,35 @@ const router = require("./app/routes/router");
 const app = express();
 app.use(express.json());
 
+// cors option localhost
+
+/*const corsOptions = {
+  credentials: true,
+  origin: "http://localhost:3000",
+};
+
+ app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+}); */
+
 const corsOptions = {
   credentials: true,
-  origin: "https://mamediatheque.netlify.app",
+  origin: process.env.CLIENT_URL,
 };
 
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://mamediatheque.netlify.app"
-  );
+  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
@@ -27,6 +44,10 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
+
+// delete on prod
+
+//app.set("trust proxy", 1);
 
 // routes
 app.use(router);
