9d5fd719d6f45cfc11a3698c5960c0861df471f1
unknown <cahuzac.p@gmail.com>
2022-01-12 17:46:29 +0100

    fix login

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -69,10 +69,10 @@ const userController = {
   login: async (req, res) => {
     try {
       const { email, password } = req.body;
-      console.log(email, password);
-      console.log(email, password);
+      console.log(email);
+      console.log(password);
       const bodyErrors = [];
-      if (!email && !password) {
+      /* if (!email && !password) {
         bodyErrors.push("Votre email ou mot de passe est vide");
       }
       if (!password) {
@@ -80,26 +80,24 @@ const userController = {
       }
       if (bodyErrors.length) {
         res.status(400).json(bodyErrors);
+
         return;
-      }
+      } */
 
       await UserModel.findOne({ email }, (err, user) => {
-        if (!password) {
-          console.log("pas de mot de passe");
-          res.status(400).json({ errorMessage: "pas de mot de passe", err });
+        if (err) {
+          res.status(500).json({ message: err });
           return;
         }
-        if (!email) {
-          res.status(404).json({ errorMessage: "pas d'email", err });
-          return;
-        }
-        if (!err && email && bcrypt.compareSync(password, user.password)) {
+        if (user && bcrypt.compareSync(password, user.password)) {
           //delete req.body;
           res.status(200).json({
             _id: user._id,
             email: user.email,
             logged: true,
+            message: "vous êtes authentifié",
           });
+
           return;
         } else {
           res.status(400).json({
